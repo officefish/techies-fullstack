72a9a8a3a68a43b1393081ae94f17b47a662917e
officefish <indicogames@yandex.ru>
2023-04-04 16:30:32 -0700

    seo module added, optimize font fix

---
+++ src/client/utilities/layout.utilite.ts
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
